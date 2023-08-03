9e87fbc512ba80e583053643c7ea4a76969408c9
Saurabh <singhsaurabh1606@gmail.com>
2023-08-03 22:42:43 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            try {
+                const result = new Function('return ' + string)();
+                string = String(result);
+                input.value = string;
+            } catch (error) {
+                // Handle error if the input is invalid
+                input.value = 'Error';
+            }
+        }
+
+        else if (e.target.innerHTML == 'AC') {
+            string = "";
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+
+    })
+})
